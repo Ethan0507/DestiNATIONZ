1de0f67dd5427c0301fa4e114bc397d303e1d264
Ethan Palani <ethanpalani@gmail.com>
2020-03-28 11:29:04 +0530

    First commit

---
+++ frontend/src/places/components/PlaceList.js
@@ -0,0 +1,41 @@
+import React, { useContext } from 'react';
+
+import Card from '../../shared/components/UIElements/Card';
+import PlaceItem from './PlaceItem';
+import Button from '../../shared/components/FormElements/Button';
+import './PlaceList.css';
+import { AuthContext } from "../../shared/context/auth-context";
+
+const PlaceList = props => {
+  const auth = useContext(AuthContext);
+  if (props.items.length === 0 && props.user === auth.userId) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>No places found. Maybe create one?</h2>
+          <Button to="/places/new">Share Place</Button>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="place-list">
+      {props.items.map(place => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          image={place.image}
+          title={place.title}
+          description={place.description}
+          address={place.address}
+          creatorId={place.creator}
+          coordinates={place.location}
+          onDelete={props.onDeletePlace}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default PlaceList;
