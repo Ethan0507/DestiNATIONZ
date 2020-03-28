e9aa99ef10861d5b8b5bec9167be34a16b28947f
Ethan Palani <ethanpalani@gmail.com>
2020-03-28 11:44:22 +0530

    API Key Removed From Code

--- backend/util/location.js
+++ backend/util/location.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = "AIzaSyCuvu7mHF5LKJhXVOC6QlanQsZOhmNOHNU";
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 async function getCoordsForAddress(address) {
   // return {
